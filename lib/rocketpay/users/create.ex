af9ccbb68bc5d79793703e32bdb1d41f57e29682
Lucas Santos <lucassm02@gmail.com>
2021-02-26 21:07:28 +0000

    chore: save changes

--- lib/rocketpay/users/create.ex
+++ lib/rocketpay/users/create.ex
@@ -1,9 +1,34 @@
 defmodule Rocketpay.User.Create do
-  alias Rocketpay.{Repo, User}
+  alias Ecto.Multi
+  alias Rocketpay.{Repo, User, Account}
 
   def call(params) do
-    params
-    |> User.changeset()
-    |> Repo.insert()
+    Multi.new()
+    |> Multi.insert(:create_user, User.changeset(params))
+    |> Multi.run(:create_account, fn repo, %{create_user: user} ->
+      insert_Account(repo, user)
+    end)
+    |> run_transaction()
+
+    # params
+    # |> User.changeset()
+    # |> Repo.insert()
+  end
+
+  defp run_transaction(mult) do
+    case Repo.transaction(mult) do
+      {:error, _operation, reason, _changes} -> {:error, reason}
+      {:ok, %{create_account: account}} -> {:ok}
+    end
+  end
+
+  defp insert_Account(repo, user) do
+    user.id
+    |> account_changeset()
+    |> repo.insert()
+  end
+
+  defp account_changeset(user_id) do
+    Account.changeset(%{user_id: user_id, balance: "0.00"})
   end
 end
